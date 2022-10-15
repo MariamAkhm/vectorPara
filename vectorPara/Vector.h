cb12e1a5f6dd97907d58614635b1c089439c782d
MariamAkhm <mahmetsafina7@gmail.com>
2022-10-15 21:35:46 +0300

    пыталась сделать верктора

--- vectorPara/Vector.h
+++ vectorPara/Vector.h
@@ -1,12 +1,15 @@
 #pragma once
+#include <iostream>
+#include <string>
 namespace VectorSpace {
 	template <typename T>
 	struct Vector {
 	protected:
-		T* data;
 		size_t n;
+		T* data;
 	public:
 		Vector(size_t n = 0) :
+			n{n},
 			data{ new T[n] } {};
 		size_t size() const
 		{
@@ -17,6 +20,13 @@ namespace VectorSpace {
 		{
 			for (size_t l = 0; l < n; ++l)
 				data[l] = v.data[l];
+		}
+		string& operator[] (size_t k, T v)
+		{
+			for (size_t i = 0; i < k; ++i)
+			{
+				return data[i];
+			}
 		}
 	};
 }
